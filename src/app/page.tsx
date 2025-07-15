0bd5660d022015e91e2f3b2a95f7abf166c84323
Kartik Garg <gargkartik503@gmail.com>
2025-07-15 13:33:13 +0530

    small change deployment fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
 'use client'
-import PricingPage from "@/components/Pricing Page/PricingPage"
+import PricingPage from "@/Components/Pricing Page/PricingPage"
 import SmoothScroll from "./SmoothScroll"
-import TestimonialsPage from '@/components/Testimonials/TestimonialsPage';
-import WhyChoosePage from '@/components/WhyChoose/WhyChoosePage';
-import LastPortal from '@/components/End/LastPortal';
-import FinalPage from '@/components/End/FinalPage';
-import NavBarAll from '@/components/NavBar/NavBarAll';
-import BountiesSection from '@/components/Bounties/BountiesSection';
-import { ThemeProvider } from '@/components/End/ThemeChangeContext';
-import NavBar3 from '@/components/NavBar/NavBar3';
-import MaskedPage from "@/components/Home/PortalReveal";
-import Footer from "@/components/Footer/Footerpage";
+import TestimonialsPage from '@/Components/Testimonials/TestimonialsPage';
+import WhyChoosePage from '@/Components/WhyChoose/WhyChoosePage';
+import LastPortal from '@/Components/End/LastPortal';
+import FinalPage from '@/Components/End/FinalPage';
+import NavBarAll from '@/Components/NavBar/NavBarAll';
+import BountiesSection from '@/Components/Bounties/BountiesSection';
+import { ThemeProvider } from '@/Components/End/ThemeChangeContext';
+import NavBar3 from '@/Components/NavBar/NavBar3';
+import MaskedPage from "@/Components/Home/PortalReveal";
+import Footer from "@/Components/Footer/Footerpage";
 
 export default function Home() {
   return (
